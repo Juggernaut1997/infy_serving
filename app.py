7067f03914dbc19d1be04b333621582eaf7c288b
Tapan Khandelwal <38181043+Juggernaut1997@users.noreply.github.com>
2020-12-30 19:28:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,8 +47,8 @@ def get_data():
     return return_data
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
-    #app.run(port=8080)
+#     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run()
         
         
     
